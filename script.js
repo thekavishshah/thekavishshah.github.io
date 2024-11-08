79bfd5b91c6f93f2d7715ea885c2ac05237d7cb0
Kavish Shah <kavishshah2005@gmail.com>
2024-11-08 02:30:54 -0700

    Updates pages

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Smooth scrolling for navigation links
 document.querySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      if (this.getAttribute('href').startsWith('#')) {
-          e.preventDefault();
-          document.querySelector(this.getAttribute('href')).scrollIntoView({
-              behavior: 'smooth'
-          });
-      }
-  });
+    anchor.addEventListener('click', function (e) {
+        if (this.getAttribute('href').startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
 });
