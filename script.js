6d037d72b606080fd1819edb2cf55d47dbd619f2
Kavish Shah <kavishshah2005@gmail.com>
2024-11-09 01:48:15 -0700

    Changed some details

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ document.querySelectorAll('nav a').forEach(anchor => {
         }
     });
 });
+
+fetch('https://api.github.com/users/thekavishshah/repos')
+    .then(response => response.json())
+    .then(repos => {
+        const projectsContainer = document.querySelector('.projects-grid');
+        repos.forEach(repo => {
+            projectsContainer.innerHTML += `
+                <div class="project-item">
+                    <img src="path/to/default-image.jpg" alt="${repo.name}">
+                    <h3>${repo.name}</h3>
+                    <p>${repo.description || 'No description available.'}</p>
+                    <a href="${repo.html_url}" class="btn">View Project</a>
+                </div>
+            `;
+        });
+    });
